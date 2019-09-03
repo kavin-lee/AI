b58814e87c3cda7cd91454220e28b7aa43c41a60
kavin-lee <kavin_lee@yeah.net>
2019-09-03 17:25:54 +0800

    多个窗口的展示

---
+++ DS/day02/01_demo_figure.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# !coding=utf-8
+
+'''
+多个窗口的展示
+'''
+import numpy as np
+
+import matplotlib.pyplot as mp
+
+mp.figure('Figure A', facecolor='gray')
+mp.figure('Figure B', facecolor='lightgray')
+mp.show()
