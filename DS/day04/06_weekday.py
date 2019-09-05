695321ef7b45cd32ec695414ff8a7df88ea33f9d
kavin-lee <kavin_lee@yeah.net>
2019-09-05 11:26:44 +0800

    简单的时间数据处理 统计每周一,周二,周三,周四,周五的数据

---
+++ DS/day04/06_weekday.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# !coding=utf-8
+'''
+简单的时间数据处理
+统计每周一,周二,周三,周四,周五的数据
+'''
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as mp
+import matplotlib.dates as md
+
+
+def dmy2wday(dmy):
+    '''日期格式的修改'''
+    dmy = str(dmy, encoding='utf-8')
+    time = dt.datetime.strptime(dmy, '%d-%m-%Y').date()
+    t = time.weekday()
+    # 修改为2011-1-1格式
+    return t
+
+
+# 接受处理后的数据结果,分别为,日期,开盘价,最高价,最低价,收盘价
+wdays, opening_prices, highest_prices, \
+lowest_prices, closing_prices, volumes \
+    = np.loadtxt('../da_data/aapl.csv',
+                 delimiter=',',
+                 usecols=(1, 3, 4, 5, 6, 7),
+                 unpack=True,
+                 dtype='f8,f8,f8,f8,f8,f8',
+                 converters={1: dmy2wday},
+                 )
+
+# 用于存储最终的结果
+ave_price = np.zeros(5)
+for wday in range(ave_price.size):
+    # 用掩码进行数据清洗
+    ave_price[wday] = np.mean(closing_prices[wdays == wday])
+print(ave_price)
